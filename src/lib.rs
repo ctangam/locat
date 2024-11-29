a6793fd637cec992bd450c2c530bb0eed3d620e9
ctangam <luochenjoey@gmail.com>
2024-11-29 08:58:17 +0000

    update

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,28 @@
 use std::net::IpAddr;
 
-pub struct Locat {}
+pub struct Locat {
+    geoip: maxminddb::Reader<Vec<u8>>,
+}
+
+#[derive(Debug, thiserror::Error)]
+pub enum Error {
+    #[error("maxminddb error: {0}")]
+    MaxMindDb(#[from] maxminddb::MaxMindDBError),
+}
 
 impl Locat {
-    pub fn new(_geoip_country_db_path: &str, _analytics_db_path: &str) -> Self {
-        Self {  }
+    pub fn new(geoip_country_db_path: &str, _analytics_db_path: &str) -> Result<Self, Error> {
+        Ok(Self { 
+            geoip: maxminddb::Reader::open_readfile(geoip_country_db_path)?,
+         })
     }
 
-    pub fn ip_to_iso_code(&self, _addr: IpAddr) -> Option<&str> {
-        None
+    pub fn ip_to_iso_code(&self, addr: IpAddr) -> Option<&str> {
+        self.geoip
+            .lookup::<maxminddb::geoip2::Country>(addr)
+            .ok()?
+            .country?
+            .iso_code
     }
 
     pub fn get_analytics(&self) -> Vec<(String, u64)> {
